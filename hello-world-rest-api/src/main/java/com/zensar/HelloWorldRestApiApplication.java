2e0cfce8c3600edff34aa61d14e946d3dcb53012
sharadrajore <42150372+sharadrajore@users.noreply.github.com>
2020-04-13 23:42:21 +0530

    Update HelloWorldRestApiApplication.java

--- hello-world-rest-api/src/main/java/com/zensar/HelloWorldRestApiApplication.java
+++ hello-world-rest-api/src/main/java/com/zensar/HelloWorldRestApiApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldRestApiApplication.class, args);
+		System.out.println("Ram");
 	}
 
 }
